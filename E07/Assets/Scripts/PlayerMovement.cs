9d9f145057d843713694d3103036854e6df1f822
alvin870203 <alvin870203@gmail.com>
2023-08-27 21:53:36 +0800

    Finish E07

---
+++ E07/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // We marked this as "Fixed"Update because we
+    // are using it to mess with physics.
+    void FixedUpdate()
+    {
+        // Add a forward force
+        // force==0 when no rb.AddForce is specified, i.e., acceleration==0 & velocity==const if no friction
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce  * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
